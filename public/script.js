c5a7883ad86498873b51f23a026a40c075fa9600
lach <lach>
2023-05-20 20:05:58 +1000

    add movies page

--- public/script.js
+++ public/script.js
@@ -1,32 +1,108 @@
-import anime from "animejs";
+import anime, { easings } from "animejs";
 
-const movieDetailsPanel = document.querySelector('#moviePage');
+const movieDetailsPage = document.querySelector('#movieDetailsPage');
+
+const movieAddPage = document.querySelector('#movieAddPage')
+const movieAddButton = document.querySelector('#addMovieButton')
+
+const mainPage = document.querySelector('#mainPage');
 const moviePageExitButton = document.querySelector('#moviePageExitButton');
 
-function openPanel(element){
-    element.style.display = '';
-    // element.style.right = '0';
+const openPages = [mainPage]
+
+function hidePage(pageElement, duration) {
+
+    if (openPages.includes(pageElement)) {
+        openPages.splice(openPages.indexOf(pageElement), 1)
+    } else {
+        console.error(`page ${pageElement.id} could not be closed (is not opened)`);
+        return;
+    }
     anime({
-        targets: element,
-        translateX: window.innerWidth,
-        duration: 300
+        targets: pageElement,
+        duration: duration,
+        opacity: 0,
+        easing: "easeInOutQuad",
+        complete: () => {
+            pageElement.style.display = 'none';
+        }
     });
+
 }
 
-function closePanel(element){
-    element.style.display = 'none';
-    element.style.right = '100%';
+function showPage(pageElement, duration) {
+    if (openPages.includes(pageElement)) {
+        console.error(`page ${pageElement.id} could not be opened (already opened)`);
+        return;
+    }
+
+    pageElement.style.display = 'block';
+
+    anime({
+        targets: pageElement,
+        duration: duration,
+        opacity: 1,
+        easing: "easeInOutQuad"
+    });
+
+    openPages.push(pageElement)
+}
+
+function hideAllPages(duration) {
+    openPages.forEach((elementID) => {
+        hidePage(elementID, duration);
+    });
 }
 
 let cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
-    console.log(card); 
     card.addEventListener('click', (event) => {
-        openPanel(movieDetailsPanel);
+        hideAllPages(120);
+        showPage(movieDetailsPage, 120);
     })
 });
 
 moviePageExitButton.addEventListener('click', (event) => {
-    closePanel(movieDetailsPanel);
+    hideAllPages(120);
+    showPage(mainPage, 120);
 });
+
+movieAddButton.addEventListener('click', (event) => {
+    hideAllPages(movieDetailsPage, 120);
+    showPage(movieAddPage, 120);
+});
+
+const multiSelectFields = document.querySelectorAll(".input-multi");
+multiSelectFields.forEach((element) => {
+
+    const inputElems = element.querySelectorAll("input");
+    const lastInputElem = inputElems[inputElems.length - 1];
+
+    const addButton = document.createElement("Button");
+    addButton.textContent = "+";
+    addButton.name = element.id;
+    addButton.classList.add('add-field-button')
+    element.append(addButton);
+
+
+    addButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const clone = lastInputElem.cloneNode();
+        clone.value = "" // reset value
+        element.insertBefore(clone, addButton);
+
+        anime({
+            targets: clone,
+            width: ["0", "200px"],
+        })
+
+        clone.addEventListener("change", (event) => {
+
+            if (clone.value.length == 0) {
+                // delete if it has no value
+                clone.remove();
+            }
+        });
+    })
+})
