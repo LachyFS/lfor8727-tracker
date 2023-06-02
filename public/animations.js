437b8af043f72f83691e98a5511bd34269d25c2b
lach <lach>
2023-06-02 12:19:13 +1000

    animejs commet

--- public/animations.js
+++ public/animations.js
@@ -1,6 +1,8 @@
 import anime from "animejs";
 
 export function errorShakeAnimation(target) {
+    // in animeJS you can pass in an object with the properties you want to animate given a target
+    // where we're animating the translateX property from 0.75em to 0 with the default easing and duration
     anime({
         targets: target,
         translateX: ["0.75em", 0],
